776e77dedddcb65e0ec3b56266f98c861c77ba68
yusufatw <yusufatw@gmail.com>
2018-09-02 02:39:36 +0700

    Update Toast output for detected the current location

--- app/src/main/java/id/cathode/maplocationdemo/MapsActivity.java
+++ app/src/main/java/id/cathode/maplocationdemo/MapsActivity.java
@@ -3,6 +3,8 @@ package id.cathode.maplocationdemo;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -12,6 +14,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -20,6 +23,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -76,6 +83,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
 
                 //Toast.makeText(getApplicationContext(), userLocation.toString(),Toast.LENGTH_SHORT).show();
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                try {
+                    List<Address> listAddress = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(),1);
+                    if (listAddress != null && listAddress.size() > 0){
+                        Log.i("Placeinfo", listAddress.get(0).toString());
+                        String address = "";
+                        if (listAddress.get(0).getSubThoroughfare() != null){
+                            address += listAddress.get(0).getSubThoroughfare() + ", ";
+                        }
+                        if (listAddress.get(0).getThoroughfare() != null){
+                            address += listAddress.get(0).getThoroughfare() + ", ";
+                        }
+                        if (listAddress.get(0).getLocality() != null){
+                            address += listAddress.get(0).getLocality() + ", ";
+                        }
+                        if (listAddress.get(0).getPostalCode() != null){
+                            address += listAddress.get(0).getPostalCode() + ", ";
+                        }
+                        if (listAddress.get(0).getCountryName() != null){
+                            address += listAddress.get(0).getCountryName();
+                        }
+
+                        Toast.makeText(getApplicationContext(), address, Toast.LENGTH_SHORT).show();
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
